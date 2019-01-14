91a505d7635fb3ca3039bb52a2d24d5311e4dafc
MyunghoBae <bmh961@naver.com>
2019-01-14 15:21:46 +0900

    [BAEKJOON] 11376. 열혈강호2
    https://www.acmicpc.net/problem/11376

---
+++ Baekjoon/11376_Gangho2.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define MAX 2001
+
+int A[MAX];
+int B[MAX];
+bool visited[MAX];
+vector<int> adj[MAX];
+
+bool dfs(int a)
+{
+	visited[a] = true;
+	for(int b: adj[a])
+	{
+		if(B[b] == -1 || !visited[B[b]] && dfs(B[b]))
+		{
+			B[b] = a;
+			A[a] = b;
+			return true;
+		}
+	}
+	return false;
+}
+int main()
+{
+	int N,M;
+	scanf("%d %d",&N,&M);
+	for(int i=0; i<N; i++)
+	{
+		int a;
+		scanf("%d",&a);
+		for(int u=0; u<a; u++)
+		{		
+			int w;
+			scanf("%d",&w);
+			adj[2*i].push_back(w-1);
+			adj[2*i+1].push_back(w-1);
+		}
+	}
+	
+	fill(A,A+N*2,-1);
+	fill(B,B+M,-1);
+	int match = 0;
+	for(int i=0; i<N*2; i++)
+	{
+		if(A[i] == -1)
+		{
+			fill(visited,visited+MAX,false);
+			if(dfs(i)) match ++;
+		}
+	}
+	printf("%d",match);
+	return 0;
+}
